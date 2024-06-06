419671be0b788f0c522d04eb73bb7af004798c59
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-06 15:55:43 +0200

    feat: add cycle js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// creo ciclo
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+
+    // settiamo condizio per fizz e buzz
+    if (i % 3 == 0 && i % 5 == 0) {
+        let fizzBuzz = ("FizzBuzz");
+        console.log(fizzBuzz);
+    } else if (i % 3 == 0) {
+        let fizz = ("Fizz");
+        console.log(fizz);
+    } else (i % 5 == 0) {
+        let buzz = ("Buzz")
+        console.log(buzz);
+    }
+}
