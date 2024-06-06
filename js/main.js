8a7c7cbb59415c54a891792d0103900e0a77a83d
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-06 16:28:53 +0200

    feat: add 100 divs with js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
+// creo costante container
+const container = document.querySelector(".container")
+
+
+
 // creo ciclo
 for (let i = 1; i <= 100; i++) {
     console.log(i);
 
-    // settiamo condizio per fizz e buzz
+    // creo costante div
+    const quad = document.createElement("div");
+
+    // aggiungo la classe default box al div
+    quad.classList.add("box")
+
+    // inserisco la costante div nel container
+    container.appendChild(quad);
+
+
+    
+    // settiamo condizioni per fizz e buzz
     if (i % 3 == 0 && i % 5 == 0) {
         let fizzBuzz = ("FizzBuzz");
         console.log(fizzBuzz);
     } else if (i % 3 == 0) {
         let fizz = ("Fizz");
         console.log(fizz);
-    } else (i % 5 == 0) {
+    } else if (i % 5 == 0) {
         let buzz = ("Buzz");
         console.log(buzz);
+    } else  {
+        
     }
 }
