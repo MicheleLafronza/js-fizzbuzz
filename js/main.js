c01eabedc8623c8e3d8e8eee814ef94b5f38b6c8
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-06 16:42:43 +0200

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // creo costante container
 const container = document.querySelector(".container")
 
-
-
 // creo ciclo
 for (let i = 1; i <= 100; i++) {
     console.log(i);
@@ -16,19 +14,24 @@ for (let i = 1; i <= 100; i++) {
     // inserisco la costante div nel container
     container.appendChild(quad);
 
-
-    
-    // settiamo condizioni per fizz e buzz
+    // settiamo condizioni per fizz e buzz con variabili da inserire nei div e le classi associate per il colore di sfondo
     if (i % 3 == 0 && i % 5 == 0) {
         let fizzBuzz = ("FizzBuzz");
+        quad.innerHTML=(fizzBuzz);
+        quad.classList.add("fizz-buzz")
         console.log(fizzBuzz);
     } else if (i % 3 == 0) {
         let fizz = ("Fizz");
+        quad.innerHTML=(fizz);
+        quad.classList.add("fizz")
         console.log(fizz);
     } else if (i % 5 == 0) {
         let buzz = ("Buzz");
+        quad.innerHTML=(buzz);
+        quad.classList.add("buzz")
         console.log(buzz);
-    } else  {
-        
+    } else {
+        quad.innerHTML=(i);
+        quad.classList.add("no-fizz-buzz")
     }
 }
